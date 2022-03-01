ed813613a5b20697ca815193312c4c7b32e1a415
smhrd <smhrd@DESKTOP-HRK4NUF>
2022-03-01 15:22:06 +0900

    asdasd

--- smartChair/src/main/java/dao/tb_communityDAO.java
+++ smartChair/src/main/java/dao/tb_communityDAO.java
@@ -76,7 +76,7 @@ public class tb_communityDAO {
 		
 		
 	}
-	public int outputCommunity(double article_seq,String article_title, String article_content, String article_file,String user_id){
+	public ArrayList<tb_communityVO> outputCommunity(double article_seq,String article_title, String article_content, String article_file,String user_id){
 		int cnt = 0;
 		try {
 			connect();
