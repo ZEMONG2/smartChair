d8ff59978cf9a36cdf650092945d19b335a4b953
ghdtjdco <ghdtjdco@DESKTOP-CHJD0PO>
2022-03-06 17:41:32 +0900

    잠시 DAO 넣는 거 수정중

--- smartChair/src/main/java/controller/ArduinoCon.java
+++ smartChair/src/main/java/controller/ArduinoCon.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.tb_poseDAO;
 import vo.tb_poseVO;
 
 /**
@@ -16,24 +17,32 @@ import vo.tb_poseVO;
 
 public class ArduinoCon extends HttpServlet {
     tb_poseVO vo = new tb_poseVO();
+    tb_poseDAO dao = new tb_poseDAO();
     String a;
 	private static final long serialVersionUID = 1L;
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		a ="a";
+		a ="0";
 		vo.setPose_type(a); 
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		 
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	      String s =request.getParameter("sensor");
-	      System.out.println(s);
-	      vo.setPose_type(s);
+		response.getWriter().append("Served at: ").append(request.getContextPath());//웹 연결
+	      String s =request.getParameter("sensor"); //센서 값 받아오기 
+	      System.out.println("현재 받아온 값 : "+s);
+	      if(!s.equals("0")) {
+	    	  if(vo.getPose_type().equals(s)) {
+	    		  System.out.println("앉기 시작");
+	    		  vo.setPose_type(s);
+	    		  vo.setPose_start_dt(null);
+	    	  }else {
+	    		  vo.setPose_end_dt(null);
+	    		  dao.date(값넣기);
+	    		 
+	    		  
+	    	  }
+	      }
 	      
-	      if(a.equals(0)&&vo.getPose_type().equals(s))
-	      
-	      vo.setPose_start_dt(null);
-
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- smartChair/src/main/java/dao/tb_poseDAO.java
+++ smartChair/src/main/java/dao/tb_poseDAO.java
@@ -10,69 +10,69 @@ import oracle.sql.DATE;
 
 public class tb_poseDAO {
 
-	PreparedStatement psmt = null;
-	Connection conn = null;
-	ResultSet rs = null;
-	public void connect() {
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			String url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524:xe";
-			String user = "campus_d_2_0216";
-			String password = "smhrd2";
-			conn = DriverManager.getConnection(url, user, password);
-			
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void close() {
-		try {
-			if(rs != null) {
-			rs.close();
-			}
-			if(psmt != null) {
-			psmt.close();
-			}
-			if(conn != null) {
-			conn.close();
-			}
-		} catch (Exception e2) {
-			e2.printStackTrace();
-		}
-	}
-	public boolean pose_start(String pose_type, Date pose_start_dt, Date pose_end_dt, String user_id) {
-		boolean cnt = false;
-		
-		String db_user_id = "";
-		try {
-			connect();
-			
-			String sql = "insert into tb_pose values(?, ?, ?, ?)";
-			
-			rs = psmt.executeQuery();
-			
-				psmt = conn.prepareStatement(sql);
-				psmt.setString(1, pose_type);
-				psmt.setDate(2, pose_start_dt);
-				psmt.setDate(3, pose_end_dt);
-				psmt.setString(4, user_id);
-				cnt = psmt.executeUpdate();
+   PreparedStatement psmt = null;
+   Connection conn = null;
+   ResultSet rs = null;
+   public void connect() {
+      try {
+         Class.forName("oracle.jdbc.driver.OracleDriver");
+         String url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524:xe";
+         String user = "campus_d_2_0216";
+         String password = "smhrd2";
+         conn = DriverManager.getConnection(url, user, password);
+         
+      }catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+   
+   public void close() {
+      try {
+         if(rs != null) {
+         rs.close();
+         }
+         if(psmt != null) {
+         psmt.close();
+         }
+         if(conn != null) {
+         conn.close();
+         }
+      } catch (Exception e2) {
+         e2.printStackTrace();
+      }
+   }
+   public boolean pose_date(String pose_type, Date pose_start_dt, Date pose_end_dt, String user_id) {
+      boolean cnt = false;
+      
+      String db_user_id = "";
+      try {
+         connect();
+         
+         String sql = "insert into tb_pose values(?, ?, ?, ?)";
+         
+         rs = psmt.executeQuery();
+         
+            psmt = conn.prepareStatement(sql);
+            psmt.setString(1, pose_type);
+            psmt.setDate(2, pose_start_dt);
+            psmt.setDate(3, pose_end_dt);
+            psmt.setString(4, user_id);
+            cnt = psmt.executeUpdate();
 
-			
-			
+         
+         
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			close();
+      } catch (Exception e) {
+         e.printStackTrace();
+      } finally {
+         close();
 
-		}
-		
-		return cnt;
-	}
+      }
+      
+      return cnt;
+   }
 
-	}
+   }
 
-	
+   
 }
