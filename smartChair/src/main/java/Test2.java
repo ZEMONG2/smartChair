ffb05692cb2c370f225b9b16981e70270766e236
smhrd <smhrd@DESKTOP-HRK4NUF>
2022-03-08 17:18:42 +0900

    test2(아두이노 제어)

--- smartChair/src/main/java/Test2.java
+++ smartChair/src/main/java/Test2.java
@@ -1,6 +1,8 @@
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,32 +14,46 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/Test2")
 public class Test2 extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+   private static final long serialVersionUID = 1L;
+   String res="";
+   String s = "";
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Test2() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+  
+   /**
+    * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+    */
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      // TODO Auto-generated method stub
+      response.setContentType("text/html; charset=UTF-8");
+      if(request.getParameter("sensor") != null){
+      s =request.getParameter("sensor");
+      }
+      System.out.println(s);
+      response.setCharacterEncoding("UTF-8");
+      response.setContentType("text/html; charset=UTF-8");
+      PrintWriter out = response.getWriter();
+      
+//      sensor = request.getParameter("sensor");
+      if(s.equals("N")){
+      res = "{\"sensor\":"+2+"}";
+      }else if(s.equals("LC")) {
+    	  res = "{\"sensor\":"+3+"}";
+      }else  {
+       	  res = "{\"sensor\":"+3+"}";
+      }
+      out.print(res);
+      
+//      out.print("{\"led\":"+led+"}");
+   }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		String s =request.getParameter("sensor");
-		System.out.println(s);
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+   /**
+    * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+    */
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      // TODO Auto-generated method stub
+      doGet(request, response);
+   }
 
 }
